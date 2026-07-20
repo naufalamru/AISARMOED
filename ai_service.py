055ac09fbd6b59d9403b4ef101ae59b8e8127266
naufalamru <naufalamru071003@gmail.com>
2026-07-20 21:57:53 +0700

    Update ai_service.py

--- ai_service.py
+++ ai_service.py
@@ -52,7 +52,7 @@ def ask_ai(prompt):
     }
 
     payload = {
-        "model": "nvidia/nemotron-3-ultra:free",
+        "model": "openai/gpt-oss-20b:free",
         "messages": [
             {
                 "role": "system",
