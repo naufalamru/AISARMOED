5b581ece08823959a624d2ca9eccc880e74a63bf
naufalamru <naufalamru071003@gmail.com>
2026-07-20 21:55:26 +0700

    Update ai_service.py

--- ai_service.py
+++ ai_service.py
@@ -52,7 +52,7 @@ def ask_ai(prompt):
     }
 
     payload = {
-        "model": "google/gemma-4-31b:free",
+        "model": "openrouter/free",
         "messages": [
             {
                 "role": "system",
